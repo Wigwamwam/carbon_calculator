7700f14e45189bb77ae5c329e2e10ee32301b29c
George Wiggin <george@sequest.io>
2022-08-08 11:32:59 +0100

    added and merged report model to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_04_135543) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_05_142505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,32 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_135543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.string "location_country"
+    t.string "location_county"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "currency"
+    t.integer "revenue"
+    t.integer "employee_count"
+    t.integer "eletricity_usage"
+    t.boolean "renewables"
+    t.boolean "owned_vehicles"
+    t.integer "quantity_vehciles"
+    t.boolean "office_size"
+    t.string "building_heating_type"
+    t.integer "building_heating_quantity"
+    t.integer "total_expenses"
+    t.integer "flight_expenses"
+    t.integer "train_expenses"
+    t.string "phone_computer_expenses"
+    t.string "legal_consultancy_expenses"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_reports_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_135543) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reports", "companies"
 end
